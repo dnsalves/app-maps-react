c51ef936f016178b97c352b5e936735df5b992c7
dnsalves <dns_alves@hotmail.com>
2018-04-04 19:24:35 -0300

    google key

--- src/maps/mapsAction.js
+++ src/maps/mapsAction.js
@@ -5,7 +5,7 @@ import Env from '../consts'
 const BASE_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 
 export function getMap(address){
-    const url = `${BASE_URL}?key=${Env.GOOGLE_KEY}&address=${address}`
+    const url = `${BASE_URL}?v=3.31&key=${Env.GOOGLE_KEY}&address=${address}`
     const request = axios.post(url)
     return {
         type: 'MAP_FETCHED',
