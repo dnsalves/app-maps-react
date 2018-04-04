606df0f56245d7dab2981d6c0b8f11948cfa7f1a
dnsalves <dns_alves@hotmail.com>
2018-04-04 19:32:37 -0300

    remove logs

--- src/maps/mapsReducer.js
+++ src/maps/mapsReducer.js
@@ -3,13 +3,9 @@ const INITIAL_STATE = { address: '', location: {} }
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'MAP_FETCHED':
-            console.log(action);
             return { ...state, location: action.payload.data.results[0].geometry.location }
         case 'DESCRIPTION_CHANGED':
-            console.log(action);
-
             return { ...state, address: action.payload }
-
         default:
             return state
     }
